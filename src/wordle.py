769dffabb1286160bf1a6ff870206691c561c6e6
Rasesh Patel <raspat1@gmail.com>
2022-01-05 02:23:58 -0300

    Create flow for finding the best guess.
    This is just the structure of how this works. The meat and potatoes is
    the process and ranking funcstions which I haven't written yet.

--- src/wordle.py
+++ src/wordle.py
@@ -1,7 +1,13 @@
 # Ugh I forgot how to write python.
 # Wait I never knew how to write python....
 
+import operator
+from random import random
+from statistics import mean
+from collections import defaultdict
+
 from .reader import Reader
+
 
 class Wordle:
   _TEST_SOLUTION_PATH = "./data/test_dict_1"
@@ -9,11 +15,37 @@ class Wordle:
   _SOLUTION_DICTIONARY_PATH = "./data/wordle_dict_1"
   _GUESS_DICTIONARY_PATH = "./data/wordle_dict_2"
 
+  # How useful was this guess for the given soluiton?
+  def get_score(guess, solution, dictionary):
+    return random()
+
+  # How good is this guess in general?
+  def process_scores(scores):
+    return mean(scores)
+
   def main():
     solutions = Reader.get_word_list(Wordle._TEST_SOLUTION_PATH)
     guesses = Reader.get_word_list(Wordle._TEST_GUESS_PATH)
-    print(solutions)
-    print(guesses)
+
+    # solutions and guesses are both guessable
+    dictionary = solutions + guesses
+
+    scores = defaultdict(list)
+
+    for solution in solutions:
+      for guess in dictionary:
+        score = Wordle.get_score(guess, solution, dictionary)
+        scores[guess].append(score)
+
+
+    # Process the scores
+    processed_scores = {word: Wordle.process_scores(result) for word, result in scores.items()}
+
+    # Best Score?
+    ranked_scores = sorted(processed_scores.items(), key=operator.itemgetter(1))
+    show = 2 # Show the top 5
+    print(ranked_scores[0:show])
+
 
 
 if __name__ == "__main__":

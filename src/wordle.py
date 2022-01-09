edb1d1d943c8001e92beb3284567e2ba7ae29d12
Rasesh Patel <raspat1@gmail.com>
2022-01-08 22:36:10 -0300

    Write results to file.
    Move previous manual results to a folder.
    Ohter intermediate commit stuff.
    Yikes we're still a mess.

--- src/wordle.py
+++ src/wordle.py
@@ -4,6 +4,8 @@
 import cProfile
 import operator
 
+from datetime import datetime
+
 from guesser import Guesser
 from guess_combinator import GuessCombinator
 from reader import Reader
@@ -19,11 +21,12 @@ _SMALL_SET = "./data/small_test_set_1"
 
 
 class Wordle:
-  def __init__(self, solution_corpus_path, guess_corpus_path, output_count=-1, use_profiler=False):
+  def __init__(self, solution_corpus_path, guess_corpus_path, output_count=-1, use_profiler=False, write_to_file=False):
     self.solution_corpus_path = solution_corpus_path
     self.guess_corpus_path = guess_corpus_path
     self.output_count = output_count
     self.profiler = ProfilerFactory.getProfiler(use_profiler)
+    self.write_to_file = write_to_file
 
   def process(self):
     try:
@@ -47,21 +50,36 @@ class Wordle:
       #     solution_corpus, full_corpus, cr_score_cache, diff_cache, self.profiler)
 
       # Show the best guesses.
-      print(ranked_scores[0:self.output_count])
+      self.write_or_print_results(ranked_scores[0:self.output_count])
+
     finally:
       self.profiler.stop()
+
+  def write_or_print_results(self, results):
+    results = result_lines = [f'\n{str(result)}' for result in results]
+
+    if self.write_to_file:
+      try:
+        output_filename = "results/results--" + str(datetime.now())
+        output_file = open(output_filename, 'w')
+        output_file.writelines(result_lines)
+      finally:
+        output_file.close()
+    else:
+      print(''.join(results))
 
 
 def main():
   # Default flags
-  solution_corpus_path = _MEDIUM_SET
-  guess_corpus_path = _MEDIUM_SET
+  solution_corpus_path = _SMALL_SET
+  guess_corpus_path = _SMALL_SET
   output_count = -1  # Set to -1 to print all entries
   use_profiler = True
+  write_to_file = False
 
   w = Wordle(solution_corpus_path,
              guess_corpus_path,
-             output_count, use_profiler)
+             output_count, use_profiler, write_to_file)
 
   w.process()
 

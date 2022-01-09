b998d0522e40caa5a4c88352d472ea58f7b260d0
Rasesh Patel <rasesh@google.com>
2022-01-09 11:43:23 -0800

    Add results of full-combinator run.

--- src/wordle.py
+++ src/wordle.py
@@ -11,7 +11,6 @@ from guess_combinator import GuessCombinator
 from reader import Reader
 from profiler import ProfilerFactory
 
-
 _TEST_SOLUTION_PATH = "./data/test_dict_1"
 _TEST_GUESS_PATH = "./data/test_dict_2"
 _SOLUTION_CORPUS_PATH = "./data/wordle_dict_1"
@@ -21,7 +20,13 @@ _SMALL_SET = "./data/small_test_set_1"
 
 
 class Wordle:
-  def __init__(self, solution_corpus_path, guess_corpus_path, output_count=-1, use_profiler=False, write_to_file=False):
+
+  def __init__(self,
+               solution_corpus_path,
+               guess_corpus_path,
+               output_count=-1,
+               use_profiler=False,
+               write_to_file=False):
     self.solution_corpus_path = solution_corpus_path
     self.guess_corpus_path = guess_corpus_path
     self.output_count = output_count
@@ -31,10 +36,9 @@ class Wordle:
   def process(self):
     try:
       self.profiler.start()
-      (solution_corpus, full_corpus) = Reader.load_lists(
-          self.solution_corpus_path,
-          self.guess_corpus_path
-      )
+      (solution_corpus,
+       full_corpus) = Reader.load_lists(self.solution_corpus_path,
+                                        self.guess_corpus_path)
 
       self.profiler.register_corpus(full_corpus)
 
@@ -44,8 +48,9 @@ class Wordle:
       self.profiler.register_cr_score_cache(cr_score_cache)
       self.profiler.register_diff_cache(diff_cache)
 
-      ranked_scores = GuessCombinator.process(
-          solution_corpus, full_corpus, cr_score_cache, diff_cache, self.profiler, 2)
+      ranked_scores = GuessCombinator.process(solution_corpus, full_corpus,
+                                              cr_score_cache, diff_cache,
+                                              self.profiler, 2)
       # ranked_scores = Guesser.get_best_guesses(
       #     solution_corpus, full_corpus, cr_score_cache, diff_cache, self.profiler)
 
@@ -56,33 +61,32 @@ class Wordle:
       self.profiler.stop()
 
   def write_or_print_results(self, results):
-    results = [f'\n{str(result)}' for result in results]
+    results = [f"\n{str(result)}" for result in results]
     results.insert(0, self.profiler._status_string())
     if self.write_to_file:
       try:
         output_filename = "results/results--" + str(datetime.now())
-        output_file = open(output_filename, 'w')
+        output_file = open(output_filename, "w")
         output_file.writelines(results)
       finally:
         output_file.close()
     else:
-      print(''.join(results))
+      print("".join(results))
 
 
 def main():
   # Default flags
-  solution_corpus_path = _MEDIUM_SET
-  guess_corpus_path = _MEDIUM_SET
+  solution_corpus_path = _SOLUTION_CORPUS_PATH
+  guess_corpus_path = _SOLUTION_CORPUS_PATH
   output_count = -1  # Set to -1 to print all entries
   use_profiler = True
   write_to_file = True
 
-  w = Wordle(solution_corpus_path,
-             guess_corpus_path,
-             output_count, use_profiler, write_to_file)
+  w = Wordle(solution_corpus_path, guess_corpus_path, output_count,
+             use_profiler, write_to_file)
 
   w.process()
 
 
 if __name__ == "__main__":
-  cProfile.run('main()')
+  cProfile.run("main()")

93e27b323c11e97b616d40c5e26dadc95ffdd62c
Rasesh Patel <raspat1@gmail.com>
2022-01-11 00:40:02 -0300

    Configure a heroku hello world.

---
+++ app/old_app.py
@@ -0,0 +1,10 @@
+# from flask import Flask
+# from flask import render_template
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#   name = "tst"
+#   return render_template('hello.html', name=name)
---
+++ cli/main.py
@@ -0,0 +1,14 @@
+from src2 import differ
+import sys
+
+
+def main():
+
+
+sys.path.insert(0, '../src2')
+
+pass
+
+
+if __name__ == '__main__':
+  main()
